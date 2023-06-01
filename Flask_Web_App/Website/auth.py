be1d1146b3ff09b7fb168fbe95f013357a3fc175
ralphg1002 <ralphg1002@hotmail.com>
2023-06-01 19:05:18 -0400

    Add login functionality

--- Flask_Web_App/Website/auth.py
+++ Flask_Web_App/Website/auth.py
@@ -10,6 +10,22 @@ auth = Blueprint('auth', __name__)
 #GET -> going to url, POST -> Sending info to server
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        #Query db and look for specific user email
+        user = User.query.filter_by(email= email).first()
+        if user:
+            #Compare stored password to entered password
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category= 'success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category= 'error')
+        else: #If user does not exist in db
+            flash('Email does not exist.', category= 'error')
+        
     return render_template('login.html', boolean = False)
 
 @auth.route('/logout')
@@ -23,8 +39,13 @@ def sign_up():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-
-        if len(email) < 4:
+        
+        user = User.query.filter_by(email= email).first()
+        #Minimum restrictions for accounts when signing up:
+        if user:
+            #Prevent use of same email for multiple accounts
+            flash('Email already exists.', category= 'error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character', category='error')
@@ -33,6 +54,7 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters', category='error')
         else:
+            #If all requirements met, add new user
             new_user = User(email= email, first_name = first_name, password= generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
